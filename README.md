11452ee7b2506e85f59741ef021c993309439f93
Alinawaz-786 <alinawaz1857@gmail.com>
2022-02-17 23:47:24 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# NFTMarketPlace
+
+# ERC721 Deploye
+Address 0xF57B2c51dED3A29e6891aba85459d600256Cf317
+
+“Non-fungible” more or less means that it’s unique and can’t be replaced with something else. For example, a bitcoin is fungible — trade one for another bitcoin, and you’ll have exactly the same thing. A one-of-a-kind trading card, however, is non-fungible. If you traded it for a different card, you’d have something completely different
+
+How do NFTs work?
+
+At a very high level, most NFTs are part of the Ethereum blockchain. Ethereum is a cryptocurrency, like bitcoin or dogecoin, but its blockchain also supports these NFTs, which store extra information that makes them work differently from, say, an ETH coin. It is worth noting that other blockchains can implement their own versions of NFTs. 
+
+# TOKENTRANSFERPROXYADDRESS
+Address 0x82d102457854c985221249f86659c9d6cf12aa72
+# TokenAddress 
+Address 0xf44cf0b9b8328adf6d899667b2427d13759087f0
+# ProtocolFeeAddress 
+Address 0xa839d4b5a36265795eba6894651a8af3d0ae2e68
+
+All Above address are use WyvernExchange Contract Constructor 
+
+
+#Hardhat Version
+Compile
+npx hardhat compile
+Deploy
+npx hardhat run --network rinkeby scripts/deploy.js
+Clean Cache
+npx hardhat clean
+Verify contract and set address  
+npx hardhat verify --network rinkeby  "_set deploye Address"
+
---
+++ hardhat.config.js
@@ -0,0 +1,69 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require("@nomiclabs/hardhat-solhint");
+
+// require("hardhat-watcher");
+
+module.exports = {
+  // defaultNetwork: "rinkeby",
+  networks: {
+    hardhat: {},
+    // rinkeby:{
+    //   url: "https://rinkeby.infura.io/v3/4913daa7178a4c77823ddea002c39d00",
+    //   accounts: ['21ece61053289747b70f0c236f8a76bcd13caf0e25be005502d78bc8290e70d6']
+    // },
+    testnet: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      accounts: ['21ece61053289747b70f0c236f8a76bcd13caf0e25be005502d78bc8290e70d6'],
+    },
+  },
+  watcher: {
+    compilation: {
+      tasks: ["compile"],
+      files: ["./contracts"],
+      verbose: true,
+    },
+    ci: {
+      tasks: ["clean", {command: "compile", params: {quiet: true}}, {
+        command: "test",
+        params: {noCompile: true, testFiles: ["testfile.ts"]}
+      }],
+    }
+  },
+  etherscan: {
+    apiKey: "VTJJ86VD6ZKVE3WQBMRE1CGP7HUM4Z8KRC"             //testnet BANance
+    // apiKey: "XIBRQWVBQ9965HWXU135TCB1HI6CRDJNWW"                //Rinby
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.6.12"
+      },{
+        version: "0.8.0"
+      },
+      {
+        version: "0.8.2"
+      },{
+        version: "0.4.22"
+      },
+      {
+        version: "0.8.4"
+      }
+    ],
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  mocha: {
+    timeout: 20000
+  }
+}
